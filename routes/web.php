d7e26a911269df001de950b2c43219f03c22946d
KevinAnthony2203 <kamosquera3@utpl.edu.ec>
2024-12-23 14:35:59 -0500

    vistas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/inicio', function () {
+    return view('inicio');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/panelPaciente', function () {
+    return view('panelPaciente');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
